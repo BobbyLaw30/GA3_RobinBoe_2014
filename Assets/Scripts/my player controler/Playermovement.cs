83310240cd1a061f75ab1bb049f1a0d2c385d89e
BobbyLaw30 <RobBoe61973@stud.noroff.no>
2024-03-19 17:12:51 +0100

    Update Playermovement.cs

--- Assets/Scripts/my player controler/Playermovement.cs
+++ Assets/Scripts/my player controler/Playermovement.cs
@@ -10,14 +10,11 @@ public class Playermovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-    public float jump = 3f;
+   
+  
+   
 
-    Vector3 velocity;
-    bool isGrounded;
-
+   
     // Start is called before the first frame update
     void Start()
 
@@ -43,17 +40,6 @@ public class Playermovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
-
-        }
-
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
 
 
 
