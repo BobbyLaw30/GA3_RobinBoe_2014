3ebe9c68634d239e8dec3b2326f00dcc9f37f7a7
BobbyLaw30 <RobBoe61973@stud.noroff.no>
2024-04-10 16:49:57 +0200

    code faen

---
+++ Assets/Scripts/my script/UI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour 
+{ 
+    public GameObject UiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+      UiObject.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider Player) 
+    {
+        if (Player.gameObject.tag == "Player") 
+        {
+            UiObject.SetActive(true);  
+            StartCoroutine("WaitForSec"); 
+
+            
+        }
+    }  
+    IEnumerator WaitForSec() 
+    {
+        yield return new WaitForSeconds(20); 
+        Destroy(UiObject); 
+        Destroy(gameObject);
+    }
+    
+        
+     
+
+    
+}
