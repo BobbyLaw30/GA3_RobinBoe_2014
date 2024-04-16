df9e479b151270d97416ad84adc24122fd053fcf
BobbyLaw30 <RobBoe61973@stud.noroff.no>
2024-04-16 14:23:04 +0200

    adding new Island and a way to get to a new scene

---
+++ Assets/Scripts/my script/CollisionTextDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionTextDisplay : MonoBehaviour
+{ 
+    public Text displayText; 
+
+    private void OnCollisionEnter(Collision collision) 
+    { 
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            displayText.text = "test test"; 
+
+        } 
+        
+    }    
+
+
+}
+   
